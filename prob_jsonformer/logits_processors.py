e0feaaf225256b7d499af18db212cdd41b5eeef0
wassname <git@wassname.org>
2024-05-11 14:50:51 +0800

    fix len

--- prob_jsonformer/logits_processors.py
+++ prob_jsonformer/logits_processors.py
@@ -28,12 +28,12 @@ class StringStoppingCriteria(StoppingCriteria):
             # because of tokens this wont work pefectly
             gen_ids = input_ids[0][self.prompt_length :]
             o = self.tokenizer.decode(gen_ids, skip_special_tokens=True)
-            # print("o", o)
+            print("o", o)
             str_l = len(o)
-            # print(
-            #     f"stopping? self.max_length={self.max_length} str_l={str_l},self.prompt_length={self.prompt_length}"
-            # )
-            if (str_l - -self.prompt_length) > self.max_length:
+            print(
+                f"stopping? self.max_length={self.max_length} str_l={str_l},self.prompt_length={self.prompt_length}"
+            )
+            if str_l > self.max_length:
                 return True
 
         return result
